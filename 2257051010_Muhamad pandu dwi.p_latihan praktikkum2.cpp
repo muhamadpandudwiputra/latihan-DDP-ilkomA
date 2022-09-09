500d1b5eb6224ea8af263b28a0d0a9a665e4a3a5
muhamadpandudwiputra <112594630+muhamadpandudwiputra@users.noreply.github.com>
2022-09-09 16:47:17 +0700

    Add files via upload

---
+++ 2257051010_Muhamad pandu dwi.p_latihan praktikkum2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float luas,r,pi = 3.14;
+	cout<<"menghitung luas lingkaran\n";
+	cout<<"masukkan jari jari\n";
+	cin>>r;
+	luas=pi*r*r ;
+	cout<<"luas lingkaran adalah :"<<luas;
+	
+	return 0;
+}
